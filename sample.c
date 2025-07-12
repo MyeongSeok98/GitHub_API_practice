0293b718efccc5822d1a2baf49aa8f973a9c84fd
MyeongSeok11 <myong2404@gmail.com>
2025-07-12 15:44:21 +0900

    C언어 더미 코드로 변경

---
+++ sample.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * @brief 주어진 문자열을 거꾸로 출력하는 더미 함수
+ * 
+ * @param str 출력할 문자열 (널 종료)
+ */
+static void print_reverse(const char *str) {
+    if (!str) return;
+
+    // 문자열 길이 계산
+    size_t len = 0;
+    while (str[len] != '\0') {
+        ++len;
+    }
+
+    // 역순으로 출력
+    for (size_t i = 0; i < len; ++i) {
+        putchar(str[len - 1 - i]);
+    }
+    putchar('\n');
+}
